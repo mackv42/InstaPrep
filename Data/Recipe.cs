67e8e68474652e9989b35a01654c4a9f41b74a44
Adam Lepley <alepley@newpeaksolutions.com>
2022-05-09 18:47:49 -0500

    Added Recipes Page created main list template

---
+++ Data/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace InstaPrep.Data
+{
+	public class Recipe
+	{
+		public string Id { get; set; } = string.Empty;
+		public string ImageUrl { get; set; } = string.Empty;
+		public string Title { get; set; } = string.Empty;
+		public string Rating { get; set; } = string.Empty;
+		public string Duration { get; set; } = string.Empty;
+		public string Effort { get; set; } = string.Empty;
+
+		public Recipe()
+		{
+			Id = Guid.NewGuid().ToString();
+		}
+	}
+}
+
